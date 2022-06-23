5354ba632ac0e08a779df2c02776241cbff90761
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-24 11:16:45 +1200

    staart on front-end

--- all-stars/src/components/Navbar.jsx
+++ all-stars/src/components/Navbar.jsx
@@ -4,15 +4,34 @@ function Navbar() {
       <h1 class="text-slate-50 text-center text-5xl text-yellow-400 font-extrabold bg-slate-400 bg-opacity-30 p-4">
         Matariki
       </h1>
-    <nav class="flex justify-center space-x-4 bg-slate-400 bg-opacity-30">
-  <a href="/dashboard" class=" font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
-  <a href="/team" class="font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
-  <a href="/projects" class="font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">Stars</a>
-  <a href="/reports" class="font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">About</a>
-</nav>
-</div>
-  );
+      <nav class="flex justify-center space-x-4 bg-slate-400 bg-opacity-30">
+        <a
+          href="/dashboard"
+          class=" font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+        >
+          Home
+        </a>
+        {/* <a
+          href="/team"
+          class="font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+        >
+          Team
+        </a> */}
+        <a
+          href="/projects"
+          class="font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+        >
+          Stars
+        </a>
+        <a
+          href="/reports"
+          class="font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+        >
+          About
+        </a>
+      </nav>
+    </div>
+  )
 }
 
-export default Navbar;
-
+export default Navbar
