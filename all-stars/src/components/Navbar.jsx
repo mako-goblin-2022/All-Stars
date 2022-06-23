2b1fad8f666f3201667c0bb37f23114a425c6f50
Rodrigo Augusto Reis <rodrigoreis@rodrigos-mbp.lan>
2022-06-23 19:18:27 +1200

    add Navbar

---
+++ all-stars/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <div>
+      <h1 class="text-slate-50 text-center text-5xl text-yellow-400 font-extrabold bg-slate-400 bg-opacity-30 p-4">
+        Matariki
+      </h1>
+    <nav class="flex justify-center space-x-4 bg-slate-400 bg-opacity-30">
+  <a href="/dashboard" class="font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+  <a href="/team" class="font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
+  <a href="/projects" class="font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">Stars</a>
+  <a href="/reports" class="font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900">About</a>
+</nav>
+</div>
+  );
+}
+
+export default Navbar;
+
