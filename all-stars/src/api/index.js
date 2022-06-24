e788fc0ae12f991e5c606cb8efa489d8e22ca7e0
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-06-24 15:28:05 +1200

    css and client side

--- all-stars/src/api/index.js
+++ all-stars/src/api/index.js
@@ -11,3 +11,12 @@ export function getStars() {
     .catch(err => console.log(err))
     // .catch(errorHandler('GET', '/v1/stars'))
 }
+
+// export function getStar() { //takes id?
+//   return request
+//     .get('/api/v1/stars/:id')
+//     .then((res) => {
+//       return(res.body)
+//     })
+//     .catch(err => console.log(err))
+// }
--- all-stars/src/components/Navbar.jsx
+++ all-stars/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       </h1>
       <nav className="flex justify-center space-x-4 bg-slate-400 bg-opacity-30">
         <a
-          href="/dashboard"
+          href="#"
           className=" font-bold font-medium px-3 py-2 text-slate-50 rounded-lg hover:bg-slate-100 hover:text-slate-900"
         >
           Home
